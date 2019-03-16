b8852e68fab4ff815fd921c9ce0ea88f66a6568d
Mihir Shah <mihir-ms>
2019-03-16 12:12:06 +0530

    UI Folder Structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Register from './components/REGISTRATION/Registration';
+import Header from './components/HEADER/Header';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Register/>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
