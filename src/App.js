1b2f4376bb9917d104780dd72c2185168735c2ee
Mihir Shah <mihir-ms>
2019-03-16 12:07:08 +0530

    Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
 import React, { Component } from 'react';
-
-import Routes from './components/Routes';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Register from './components/REGISTRATION/Registration';
+import Header from './components/HEADER/Header';
 
 class App extends Component {
   
   render() {
     return (
-      // <Form inline>
-      //   <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-      //     <Label for="exampleEmail" className="mr-sm-2">
-      //       Email
-      //     </Label>
-      //     <Input
-      //       type="email"
-      //       name="email"
-      //       id="exampleEmail"
-      //       placeholder="something@idk.cool"
-      //     />
-      //   </FormGroup>
-      //   <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-      //     <Label for="examplePassword" className="mr-sm-2">
-      //       Password
-      //     </Label>
-      //     <Input
-      //       type="password"
-      //       name="password"
-      //       id="examplePassword"
-      //       placeholder="don't tell!"
-      //     />
-      //   </FormGroup>
-      //   <Button>Submit</Button>
-      // </Form>
-
       <div>
-        <Routes/>
-
+        <Register/>
       </div>
-
+      
     );
   }
 }
 
 export default App;
-
-
-
-// first name middle and last birthday gender spouse (yes/no) children (yes/no) parents(compulsory)
-// options ( self/ spouse/ parents/ chiledern/ )
